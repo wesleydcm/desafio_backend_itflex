6a20a4bc8dd89c9a95c8738ebbbc986287d148e7
wesleydcm <wesleycostamatos@gmail.com>
2021-10-13 09:09:56 -0300

    deploy heroku

--- migrations/versions/db3d850bc3cd_.py
+++ migrations/versions/db3d850bc3cd_.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'db3d850bc3cd'
+revision = 'dfc21a0578d6'
 down_revision = None
 branch_labels = None
 depends_on = None
